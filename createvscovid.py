04a9d6d36a70d319485e8347717d6dd13b3fe4c0
cyborg48 <cyborg414781@gmail.com>
2020-04-08 22:09:51 -0400

    added random starting point to reduce chance of double retweet

--- createvscovid.py
+++ createvscovid.py
@@ -5,6 +5,7 @@ import json
 import time
 import toneanalyzer
 import time
+import random
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -58,6 +59,8 @@ class RetweetListener(tweepy.StreamListener):
                     global retweeted_ids
 
                     getTweet = self.api.search(q="i'm%20scared OR i'm%20worried OR i'm%20stressed OR i'm%20upset OR i'm%20terrified", count=500000, lang='en')
+                    cutoff = random.randint(0, 400000)
+                    getTweet = getTweet[cutoff:]
 
                     tweeted = False
 
